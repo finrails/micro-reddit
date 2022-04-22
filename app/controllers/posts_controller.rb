3c94f9692bc4d4ce18fbbcfd1cdc46fbcbaaa703
Vitor Souza <macacofinn@yahoo.com>
2022-04-22 12:35:55 -0400

    upt(controllers): post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,23 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def create
+    @post = Post.new(post_params)
+    if User.where(id: post_params[:user_id]).present? && @post.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :user_id)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5, maximum: 240 }
+  validates :body, presence: true, length: { minimum: 5, maximum: 500 }
+  validates :user_id, presence: true
+
   belongs_to :user
   has_many :comments
 end
